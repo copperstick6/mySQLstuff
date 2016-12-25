0c62e1bd7a44858510a247079cb1341a6fe1e66f
copperstick6 <copperstick6@gmail.com>
2016-12-25 03:40:15 -0600

    added getting started!

--- beginning.py
+++ beginning.py
@@ -1,3 +1,10 @@
+import keys
 import pymysql
 pymysql.install_as_MySQLdb()
 import MySQLdb
+connection = pymysql.connect(host='localhost',
+                             user='copperstick6',
+                             password=keys.sqlKey(),
+                             db= "sys",
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
