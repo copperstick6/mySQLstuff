9734d374db555818ad00c4ed6848069cafe9f580
copperstick6 <copperstick6@gmail.com>
2017-01-04 19:38:49 -0600

    finished up SQL syntax for insertion

--- beginning.py
+++ beginning.py
@@ -10,8 +10,11 @@ connection = pymysql.connect(host='localhost',
                              cursorclass=pymysql.cursors.DictCursor)
 
 with connection.cursor() as cursor:
-    sql = "SELECT * FROM users"
+    sql = "INSERT INTO users (id, email, password) VALUES ('3', 'tested@tested.com', 'KappaPride');"
     cursor.execute(sql)
+    connection.commit()
+    getData = "SELECT * FROM users"
+    cursor.execute(getData)
     result = cursor.fetchall()
     for record in result:
         print("id: " + str(record['id']) + " email: " + str(record['email']) + " password: " + str(record['password']))
